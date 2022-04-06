c7a0a9333d45833022742f241344a741b46e6c47
ShahidSalim-oss <70953311+ShahidSalim-oss@users.noreply.github.com>
2022-04-06 11:10:27 +0200

    Update message.js

--- message.js
+++ message.js
@@ -1,1 +1,17 @@
 console.log('Hi from Pakistan and Germany. Shahid test');
+
+if (window.location.href.indexOf("franky") > -1) {
+} else if (window.location.href.indexOf("/product/") > -1) {
+  console.log("product page");
+} else if (window.location.href.indexOf("/product-category/") > -1) {
+  console.log("category page");
+} else if (window.location.href.indexOf("/cart/") > -1) {
+  console.log("cart page");
+} else if (window.location.pathname.split('/').length <2) {
+  console.log("home page");
+} else {
+  console.log("something else");
+}
+
+
+
