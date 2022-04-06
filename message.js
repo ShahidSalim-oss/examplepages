3cf3a3066d15af66c5d8c2c925fe0b7cac741544
ShahidSalim-oss <70953311+ShahidSalim-oss@users.noreply.github.com>
2022-04-06 13:25:54 +0200

    Update message.js

--- message.js
+++ message.js
@@ -3,8 +3,8 @@ console.log('Hi from Pakistan and Germany. Shahid test');
 if (window.location.href.indexOf("franky") > -1) {
 } else if (window.location.href.indexOf("/product/") > -1) {
   console.log("product page");
-  let listItems = document.querySelectorAll('div:nth-child(1) > div.row.product-image-summary-wrap > div > div > div.col-lg-6.col-12.col-md-6.summary.entry-summary > div > form > button');
-console.log(listItems);
+  let listItems = document.querySelectorAll('[data-foo="add-to-cart"]');
+   console.log(listItems);
 } else if (window.location.href.indexOf("/product-category/") > -1) {
   console.log("category page");
 } else if (window.location.href.indexOf("/cart/") > -1) {
